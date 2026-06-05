ae96cfe27f024b70473d39b67b95d6164788c7c4
Mahadisomrat <mdmahadisomrat@gmail.com>
2026-06-06 00:01:04 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from supabase import create_client, Client
 
 app = Flask(__name__)
 app.secret_key = 'chatbd_secret_2024'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading', ping_timeout=60, ping_interval=25)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading', ping_timeout=60, ping_interval=25, allow_upgrades=False)
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
